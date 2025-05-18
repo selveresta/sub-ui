928396807f3ab75ef27eaba21671d63e5a025b14
Artem <artem.lypnyk@infotech.gov.ua>
2025-05-18 22:59:44 +0300

    change url

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./App.css";
 
-const BACKEND_URL = "http://localhost:3001/api";
+const BACKEND_URL = "/api/subscribe";
 
 function App() {
 	const [email, setEmail] = useState("");
